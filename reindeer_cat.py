57e3df6b25549ad8ba7609fedf6ec0125e81797c
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-06-06 19:00:01 +0300

    add reindeer cat game

---
+++ reindeer_cat.py
@@ -0,0 +1,29 @@
+print("-------------------------------------------")
+print("Hey Client! We are going to draw Reindeer or Cat")
+print("")
+
+answer = input("Do you want to draw a Reindeer or Cat? hint type \"Reindeer\" or \"Cat\" ")
+
+if answer == "Reindeer":
+  print("Okay! Here is your Reindeer")
+  print("-------------------------------------------")
+  print(" \_\_    _/_/ ")
+  print("    \_ _/ ")
+  print("   (0 0)\_____     ")
+  print("   (___)\     )\/\/ ")
+  print("         ||   ||     ")
+  print("-------------------------------------------")
+elif answer == "Cat":
+  print("Okay! Here is your Cat")
+  print("-------------------------------------------")
+  print(" /\_/\ ")
+  print("( o.o )")
+  print(" > ^ < ")
+  print("-------------------------------------------")
+else:
+  print("------------------------------------------")
+  print("------------------------------------------")
+  print("Sorry, Put in a valid answer")
+  print("------------------------------------------")
+  print("------------------------------------------")
+  
